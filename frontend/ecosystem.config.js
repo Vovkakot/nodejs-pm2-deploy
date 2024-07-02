b198e97a679ad09ae7c746ed2daa126a88d6126e
Vovkakot <vovakot332@gmail.com>
2024-07-02 17:12:24 +0300

    feat: env example

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF } =
+  process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "mesto",
+      script: "dist/app.js",
+    },
+  ],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      "post-deploy": "cd frontend && pwd && npm ci && npm run build",
+    },
+  },
+};
