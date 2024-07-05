506b3936a7d511b008674ccbcbd0177860857e71
vovakot332@gmail.com <vovakot332@gmail.com>
2024-07-05 16:32:34 +0300

    fix: change base url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.katasonovvi.nomorepartiesco.ru/');
+  const api = new Api('https://api.katasonovvi.nomorepartiesco.ru');
 
   export default api;
